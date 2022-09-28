c045555a797fe4e89ea87e2845d13881540a4acb
Anders Flotten <anders3835@hotmail.com>
2022-09-28 00:17:23 +0000

    completed letter positions function

---
+++ letterPositions.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  for (i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      
+      return false, `🛑🛑🛑 Assertion failed: These arrays do not match!`;
+    }
+  }
+  
+  return true, `✅✅✅ Assertion passed: These arrays match!`;
+};
+
+
+
+
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  sentence = sentence.replace(" ","") //remove spaces
+  for(let i = 0; i < sentence.length; i++) {
+    if (!results[sentence[i]]) { //use ! to determine if we haven't seen the character yet
+      results[sentence[i]] = [i]; //push the index of our character location to the results object
+    } else {
+      results[sentence[i]].push(i); //if we have seen it, push the index of the next appearance to the same key. 
+    }
+  }
+  return results;
+};
