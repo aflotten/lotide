5316407ec1c575477d8e45c11be0dfa1f54c2226
Anders Flotten <anders3835@hotmail.com>
2022-09-30 22:14:21 +0000

    refactored eq arrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,11 @@
-const eqArrays = function(array1, array2) {
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
   for (i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      
-      return false, `🛑🛑🛑 Assertion failed: These arrays do not match!`;
+      return false;
     }
   }
-  
-  return true, `✅✅✅ Assertion passed: These arrays match!`;
+  return true;
 };
