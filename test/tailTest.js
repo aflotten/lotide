7f506e92a670fdfbc2a4930d1e2acf1d9e85b8cc
Anders Flotten <anders3835@hotmail.com>
2022-10-13 03:16:44 +0000

    refactored tail testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,19 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const eqArrays = require("../eqArrays");
 const tail = require("../tail");
+const assert = require("chai").assert;
 
-const array = [1, 2];
-console.log(assertArraysEqual(tail(array), [2]));
+
+describe("#Tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.isTrue(eqArrays(tail([1, 2, 3]), [2, 3]));
+  });
+
+  it("returns [] for ['5']", () => {
+    assert.isTrue(eqArrays(tail(['5']), []));
+  });
+
+  it("return fail when given full array", () => {
+    assert.isFalse(eqArrays(tail([1, 2, 3, 4]), [1, 2, 3, 4]));
+  });
+
+});
